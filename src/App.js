953922ca7e2c54489da7c1aff9d399066f6e76fd
Mayank Raghuvanshi <68193012+msr157@users.noreply.github.com>
2022-04-06 00:28:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Restaurant from "./Components/Basics/Restaurant";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Restaurant />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Basics/menuApi.js
@@ -0,0 +1,96 @@
+// const Menu = [
+//   {
+//     id: 1,
+//     image: "shdj",
+//     name: "maggi",
+//     category: "lunch",
+//     price: 25,
+//     description: "loremgsfdhd",
+//   },
+// ];
+// export default Menu;
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg", //image is loaded here from public >> Images folder
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoda.jpg",
+    name: "allu pakoda",
+    category: "brunch",
+    price: "20₹",
+    description:
+      " Aloo pakora is a popular Indian snack where sliced potatoes are dipped in spiced gram flour batter and deep fried to perfection.",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "The seeds, which are also known as corn, are used as food for humans and livestock and as a source of biofuel and can be processed into a",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "Chola is prepared by cooking chickpeas and adding spices such as cumin, coriander seeds, turmeric powder, and chili powder. Onion, garlic, and ginger are also added for additional flavor. Bhature is prepared by combining flour, salt, and oil, and kneading the dough.",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "brunch",
+    price: "80₹",
+    description:
+      "Italian dish made typically of flattened bread dough spread with a savory mixture usually including tomatoes and cheese and often other toppings and baked.",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Non-Veg Thali / Non-Vegetarian Thali - Chicken Roast ; Coconut (sliced). 2 tbs(optional) ; Curry Leaves/karivepillai. 2 sprigs ; Fennel seeds/sombu. 1/4 tsp.",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "This is a list of Indian sweets and desserts, also called mithai, a significant element in Indian cuisine. Indians are known for their unique taste and ...",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description: "Rajma chawal is kidney beans served with boiled rice.",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samosa",
+    category: "brunch",
+    price: "10₹",
+    description:
+      "A samosa (/səˈmoʊsə/) is a fried or baked pastry with a savory filling, including ingredients such as spiced potatoes, onions, peas. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
+  },
+];
+
+export default Menu;
